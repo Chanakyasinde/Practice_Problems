dcdae817f3d847c3d95ce61214cf09cdb02e74ba
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-12 22:16:31 +0530

    Update js_calculator.js

--- JavaScript/js_calculator.js
+++ JavaScript/js_calculator.js
@@ -1,1 +1,28 @@
-
+switch (sign) {
+    case "+":
+    result = a+b
+    break;
+    case "-":
+    result = a-b
+    break;
+    case "*":
+    result = a*b
+    break;
+    case "/":
+    if (b===0){
+        result = "Invalid operation"
+    } else{
+        result = a/b
+    }
+    break;
+    case "%":
+    if (b===0){
+        result = "Invalid operation"
+    } else{
+        result=a%b
+    }
+    break;
+    default:
+    result = "Invalid operation"
+}
+console.log(result)
