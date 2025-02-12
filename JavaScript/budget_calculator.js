0dff3d85def8b051e2f4a33a47867e0a0f0a3e53
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-13 00:39:14 +0530

    Update budget_calculator.js

--- JavaScript/budget_calculator.js
+++ JavaScript/budget_calculator.js
@@ -1,1 +1,21 @@
+//Write your code here
+let Total_monthly_income = parseInt(a);
+let Rent = parseInt(b);
+let Food_expenses = parseInt(c);
+let Transport_expenses = parseInt(d);
+let Other_expenses = parseInt(e);
 
+Total_expenses = Rent + Food_expenses + Transport_expenses + Other_expenses
+Remaining_Balance = Total_monthly_income - Total_expenses
+let result = 20/100*Total_monthly_income
+
+console.log("Total Income:",Total_monthly_income);
+console.log("Total Expenses:",Total_expenses);
+console.log("Remaining Balance:",Remaining_Balance);
+if (result<=Remaining_Balance){
+    console.log("You saved well this month!")
+} else if (result>=Remaining_Balance && Remaining_Balance>0){
+    console.log("Try to save more next month.")
+} else {
+    console.log("You have overspent!")
+}
