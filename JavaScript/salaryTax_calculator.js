fa58d4df730fb74655cf6a27fb2e1f1f65923172
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-13 00:42:15 +0530

    Update salaryTax_calculator.js

--- JavaScript/salaryTax_calculator.js
+++ JavaScript/salaryTax_calculator.js
@@ -1,1 +1,14 @@
-
+function findTax(salary) {
+    switch(true){
+        case(salary<=0):
+        return "Salary not valid"
+        case(salary>0 && salary<=500000):
+        return 0
+        case(salary>500000 && salary<=1000000):
+        return 0.1*salary
+        case(salary>1000000 && salary<=1500000):
+        return 0.2*salary
+        case(salary>1500000):
+        return 0.3*salary
+    }
+}
