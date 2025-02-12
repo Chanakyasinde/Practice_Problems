09c5ddfdc59dee6a0d5a4a3211bb00b05e742eac
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-12 22:19:12 +0530

    Update house_of_robbers.py

--- Python/house_of_robbers.py
+++ Python/house_of_robbers.py
@@ -1,1 +1,13 @@
+def recurse(nums, i):
+    # base case
+    if i>=len(nums):
+        return 0
+    # recursive case
+    ans_i_1 = recurse(nums,i+1)
+    ans_i_2 = recurse(nums,i+2)
+    chori = nums[i]+ans_i_2
+    chori_nah = 0 + ans_i_1
+    return max(chori, chori_nah)
 
+def rob(nums):
+    return recurse(nums,0)
