a546355feb99aaeb5737d9f89b46610f307ce72e
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-13 00:39:34 +0530

    Update Ecommerce_calculator.js

--- JavaScript/Ecommerce_calculator.js
+++ JavaScript/Ecommerce_calculator.js
@@ -1,1 +1,10 @@
+// Your code here
+let discount_Amount=(originalPrice*discountPercentage) / 100;
+let finalPrice = originalPrice - discount_Amount;
 
+console.log("Original Price:",originalPrice);
+console.log("Discount Amount:",discount_Amount);
+console.log("Final Price:",finalPrice);
+if (discountPercentage > 50){
+    console.log("Great Deal!")
+}
