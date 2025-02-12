0fcb50db9829679b3817601d0ddbec679e7d63b3
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-13 00:38:49 +0530

    Update dataType_logicalOperators.js

--- JavaScript/dataType_logicalOperators.js
+++ JavaScript/dataType_logicalOperators.js
@@ -1,1 +1,9 @@
-
+let condition1= (typeof input1 === "number" && input1>20);
+let condition2= (typeof input2 === "boolean" && input2===true);
+if (condition1 && condition2) {
+    console.log("All conditions are met.")
+} else if (condition1 || condition2) {
+    console.log("At least one condition is met.")
+} else {
+    console.log("No conditions are met.")
+}
