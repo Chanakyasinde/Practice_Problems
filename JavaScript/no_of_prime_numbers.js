35eb3c3dc2c947eca855c0897f82ec14631e26c9
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-14 02:45:46 +0530

    Update no_of_prime_numbers.js

--- JavaScript/no_of_prime_numbers.js
+++ JavaScript/no_of_prime_numbers.js
@@ -1,1 +1,18 @@
+let primeCount = 0;
 
+for (let num = start; num <= end; num++) {
+    let isPrime = true;
+
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime && num > 1) {
+        primeCount++;
+    }
+}
+
+console.log(primeCount);
