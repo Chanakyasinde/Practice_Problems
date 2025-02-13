160c2ce35a7c0b48c21e308d45253d3881b0c742
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-14 02:46:08 +0530

    Update diamond_pattern.js

--- JavaScript/diamond_pattern.js
+++ JavaScript/diamond_pattern.js
@@ -1,1 +1,31 @@
+function printDiamondPattern(n) {
+    // Top half of the diamond (including the middle row)
+    for (let i = 0; i < n; i++) {
+        // Print leading spaces
+        let spaces = " ".repeat(2 * (n - i - 1));
+        process.stdout.write(spaces);
+        
+        // Print stars
+        let stars = "* ".repeat(2 * i + 1);
+        process.stdout.write(stars);
 
+        // Move to the next line
+        console.log();
+    }
+
+    // Bottom half of the diamond (excluding the middle row)
+    for (let i = 0; i < n - 1; i++) {
+        // Print leading spaces
+        let spaces = " ".repeat(2 * (i + 1));
+        process.stdout.write(spaces);
+        
+        // Print stars
+        let stars = "* ".repeat(2 * (n - i - 1) - 1);
+        process.stdout.write(stars);
+
+        // Move to the next line
+        console.log();
+    }
+}
+
+printDiamondPattern(n);
