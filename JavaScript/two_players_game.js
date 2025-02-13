dac84902ee478880de9b0cda2bb92a98599dbfdc
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-14 02:46:34 +0530

    Update two_players_game.js

--- JavaScript/two_players_game.js
+++ JavaScript/two_players_game.js
@@ -1,1 +1,13 @@
-
+function findWinner(scores) {
+  for (let i=0; i<scores.length; i++){
+    if (i%2==0){
+      if (scores[i]<0){
+        return "Arun";
+      }
+    } else {
+      if (scores[i]<0){
+        return "Naman";
+      }
+    }
+  }
+}
