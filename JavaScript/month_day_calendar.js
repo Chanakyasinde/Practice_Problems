079f8c7f8d4f4ecb05d0c0d5c7ddbfcb3e1b8129
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-14 02:45:14 +0530

    Update month_day_calendar.js

--- JavaScript/month_day_calendar.js
+++ JavaScript/month_day_calendar.js
@@ -1,1 +1,41 @@
-
+switch(month){
+    case "january":
+    console.log("31 days")
+    break;
+    case "february":
+    console.log("28 or 29 days (leap year)")
+    break;
+    case "march":
+    console.log("31 days")
+    break;
+    case "april":
+    console.log("30 days")
+    break;
+    case "may":
+    console.log("31 days")
+    break;
+    case "june":
+    console.log("30 days")
+    break;
+    case "july":
+    console.log("31 days")
+    break;
+    case "august":
+    console.log("31 days")
+    break;
+    case "september":
+    console.log("30 days")
+    break;
+    case "october":
+    console.log("31 days")
+    break;
+    case "november":
+    console.log("30 days")
+    break;
+    case "december":
+    console.log("31 days")
+    break;
+    default:
+    console.log("Invalid month")
+    break;
+}
