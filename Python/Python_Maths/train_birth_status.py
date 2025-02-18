7faf786a1b30a955efb57a77e4c48c050cc64e19
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-18 19:48:49 +0530

    Update train_birth_status.py

--- Python/Python_Maths/train_birth_status.py
+++ Python/Python_Maths/train_birth_status.py
@@ -1,1 +1,11 @@
-
+N=int(input())
+if N%8== 1 or N%8==4:
+    print("Lower")
+elif N%8== 2 or N%8==5:
+    print("Middle")
+elif N%8== 3 or N%8==6:
+    print("Upper")
+elif N%8== 7:
+    print("Side Lower")
+else:
+    print("Side Upper")
