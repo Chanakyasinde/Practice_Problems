66ff4212429efcfe55c177a7d0ba538d20731624
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-21 17:49:26 +0530

    Update implement_bubble_sort.py

--- Python/implement_bubble_sort.py
+++ Python/implement_bubble_sort.py
@@ -1,1 +1,7 @@
-
+def bubbleSort(seq):
+    n=len(seq)
+    for i in range(n-1):
+        for j in range(1,n):
+            if seq[j-1]>seq[j]:
+                seq[j-1],seq[j]=seq[j],seq[j-1]
+    return seq
