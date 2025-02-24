ac2d758185a8c1613385723158d83afb69f451f9
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-24 19:40:55 +0530

    Update modify_page_count.py

--- Python/modify_page_count.py
+++ Python/modify_page_count.py
@@ -1,1 +1,10 @@
+n = int(input(""))
 
+book_catalog = {}
+
+for _ in range(n):
+    book_title = input("")
+    page_count = int(input(""))
+    book_catalog[book_title] = page_count - 100
+
+print(book_catalog)
