f43f0dca30fab6b2fff0545b2cdc063c9968f985
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-24 19:41:57 +0530

    Update access_author_books.py

--- Python/access_author_books.py
+++ Python/access_author_books.py
@@ -1,1 +1,12 @@
+n = int(input())
 
+authors = []
+
+
+for _ in range(n):
+    book_name = input()  # Read the book name (not used further)
+    author_name = input()  # Read the author's name
+    authors.append(author_name)  # Store the author's name in the list
+
+
+print(", ".join(authors))
