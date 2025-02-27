9b368486dce7f20abfa58f8316f413ebf2cf8be6
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-28 00:41:12 +0530

    Update js_authentication.js

--- JavaScript/js_authentication.js
+++ JavaScript/js_authentication.js
@@ -1,1 +1,12 @@
-
+function authenticateUser(username, password, users)
+{
+return new Promise((resolve,reject)=> {
+    setTimeout(()=> {
+        if (users[username] && users[username] === password) {
+        resolve(`Login successful for user: ${username}`);
+      } else {
+        reject("Authentication failed. Invalid username or password.");
+      }
+    },3000)
+})
+}
