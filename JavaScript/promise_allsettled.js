59217fce98ab1597768a37fc4df37f08eca073f2
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-28 00:42:58 +0530

    Update promise_allsettled.js

--- JavaScript/promise_allsettled.js
+++ JavaScript/promise_allsettled.js
@@ -1,1 +1,8 @@
-
+Promise.myAllSettled = function (arr) {
+    const myarr = arr.map((promise) =>
+      Promise.resolve(promise)
+        .then((val) => ({ status: "fulfilled", value: val }))
+        .catch((err) => ({ status: "rejected", reason: err }))
+    );
+    return Promise.all(myarr);
+  }
