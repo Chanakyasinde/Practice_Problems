89c1fdc4c62ad264e573a9a04ca56cacb2d286ca
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-27 17:35:05 +0530

    Update merge_array.py

--- Python/merge_array.py
+++ Python/merge_array.py
@@ -1,1 +1,22 @@
+n,m=map(int,input().split())
+arr1=list(map(int,input().split()))
+arr2=list(map(int,input().split()))
+p1=0
+p2=0
+result=[]
+while p1<n and p2<m:
+    if arr1[p1]<=arr2[p2]:
+        result.append(arr1[p1])
+        p1+=1
+    else:
+        result.append(arr2[p2])
+        p2+=1
+while p1<n:
+    result.append(arr1[p1])
+    p1+=1
+while p2<m:
+    result.append(arr2[p2])
+    p2+=1
+print(*result)
 
+# time space and total complexity i = o(n+m)
