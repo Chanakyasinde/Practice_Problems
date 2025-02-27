ab8129c15f49f816595dd4903974fe2d5728c288
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-27 17:35:26 +0530

    Update bubble_sort.py

--- Python/Recursion/bubble_sort.py
+++ Python/Recursion/bubble_sort.py
@@ -1,1 +1,15 @@
-
+def minion(arr,i,n):
+     if i==n-1:
+          return arr
+     result = minion(arr,i+1,n)
+     mario(result,i)
+     return result
+def mario(arr,i):
+     if i==len(arr)-1:
+          return
+     if arr[i]>arr[i+1]:
+          arr[i],arr[i+1] = arr[i+1],arr[i]
+     mario(arr,i+1)
+def recursive_bubble(lst):
+     ans = minion(lst,0,len(lst))
+     return ans
