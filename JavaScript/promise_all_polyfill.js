000ed84e044b499fc4f5fabed5493aece5e9148e
Chanakya Sinde <chanakyasinde@gmail.com>
2025-02-27 17:35:57 +0530

    Update promise_all_polyfill.js

--- JavaScript/promise_all_polyfill.js
+++ JavaScript/promise_all_polyfill.js
@@ -1,1 +1,18 @@
-
+Promise.myAll = function (arr) {
+    return new Promise((res, rej) => {
+      const result = [];
+      let completed = 0;
+      for (let i = 0; i < arr.length; i++) {
+        const promise = arr[i];
+        Promise.resolve(promise)
+          .then((data) => {
+            result[i] = data;
+            completed++;
+            if (completed === arr.length) {
+              res(result);
+            }
+          })
+          .catch((err) => rej(err));
+      }
+    });
+  }
