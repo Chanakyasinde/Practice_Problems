a715f06f8d68aae276a126c4b524b9565f4c51b4
Chanakya Sinde <chanakyasinde@gmail.com>
2025-03-02 18:08:29 +0530

    Update matrix_spiral.py

--- Python/Python_Maths/matrix_spiral.py
+++ Python/Python_Maths/matrix_spiral.py
@@ -1,1 +1,30 @@
+def print_spiral_order(matrix):
+    top, bottom, left, right = 0, len(matrix) - 1, 0, len(matrix[0]) - 1
+    spiral_order = []
 
+    while top <= bottom and left <= right:
+        for i in range(left, right + 1):
+            spiral_order.append(matrix[top][i])
+        top += 1
+
+        for i in range(top, bottom + 1):
+            spiral_order.append(matrix[i][right])
+        right -= 1
+
+        if top <= bottom:
+            for i in range(right, left - 1, -1):
+                spiral_order.append(matrix[bottom][i])
+            bottom -= 1
+
+        if left <= right:
+            for i in range(bottom, top - 1, -1):
+                spiral_order.append(matrix[i][left])
+            left += 1
+
+    print(" ".join(map(str, spiral_order)))
+
+
+
+
+
+print_spiral_order(matrix)
