7d43ad7baa219c626cda4faa0a22239662f09ab1
Chanakya Sinde <chanakyasinde@gmail.com>
2025-03-02 18:12:32 +0530

    Update balance_binary_string.py

--- Python/Python_Maths/balance_binary_string.py
+++ Python/Python_Maths/balance_binary_string.py
@@ -1,1 +1,10 @@
+import math
 
+def count_balanced_binary_strings(N):
+    if N % 2 != 0:
+        return 0
+    k = N // 2
+    return math.comb(2 * k, k)
+
+N = int(input().strip())
+print(count_balanced_binary_strings(N))
