3c2fa4c7e4eabeb637dcdffa979942bb5bf0e7dd
Chanakya Sinde <chanakyasinde@gmail.com>
2025-03-06 22:01:28 +0530

    Update minimum_size_sum_subarray.py

--- Python/minimum_size_sum_subarray.py
+++ Python/minimum_size_sum_subarray.py
@@ -1,1 +1,20 @@
+target = int(input())
+nums = list(map(int, input().split()))
 
+n = len(nums)
+left = 0
+sum = 0
+minLength = float('inf')
+
+for right in range(n):
+    sum += nums[right]
+    
+    while sum >= target:
+        minLength = min(minLength, right - left + 1)
+        sum -= nums[left]
+        left += 1
+
+if minLength == float('inf'):
+    print(0)
+else:
+    print(minLength)
