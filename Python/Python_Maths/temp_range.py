6dfb60e91165dbf90525ec3cdbe31f60eab81478
Chanakya Sinde <chanakyasinde@gmail.com>
2025-03-07 23:25:55 +0530

    Update temp_range.py

--- Python/Python_Maths/temp_range.py
+++ Python/Python_Maths/temp_range.py
@@ -1,1 +1,7 @@
-
+n = int(input())
+temperatures = []
+for _ in range(n):
+    temperature = float(input())
+    temperatures.append(temperature)
+temperature_range = max(temperatures) - min(temperatures)
+print(f"{temperature_range:.1f}")
