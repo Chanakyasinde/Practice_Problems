f5b78bd20d73d5c527088c91b5bf863591229ced
Chanakya Sinde <chanakyasinde@gmail.com>
2025-03-07 23:27:20 +0530

    Update transpose_matrix.py

--- Python/Python_Maths/transpose_matrix.py
+++ Python/Python_Maths/transpose_matrix.py
@@ -1,1 +1,8 @@
+def transpose_matrix(n, m, matrix):
+    transposed = [[matrix[i][j] for i in range(n)] for j in range(m)]
+    for row in transposed:
+        print(" ".join(map(str, row)))
 
+n, m = map(int, input().split())  
+matrix = [list(map(int, input().split())) for _ in range(n)]  
+transpose_matrix(n, m, matrix)
