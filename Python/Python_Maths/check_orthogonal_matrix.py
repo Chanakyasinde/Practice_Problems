33d7e87070add0b3a7e2eee83af1811a100d4753
Chanakya Sinde <chanakyasinde@gmail.com>
2025-03-07 23:28:41 +0530

    Update check_orthogonal_matrix.py

--- Python/Python_Maths/check_orthogonal_matrix.py
+++ Python/Python_Maths/check_orthogonal_matrix.py
@@ -1,1 +1,18 @@
+def is_orthogonal(n, matrix):
+    transpose = [[matrix[j][i] for j in range(n)] for i in range(n)]
+    result = [[sum(matrix[i][k] * transpose[k][j] for k in range(n)) for j in range(n)] for i in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if i == j and result[i][j] != 1:
+                return False
+            elif i != j and result[i][j] != 0:
+                return False
+    
+    return True
 
+n = int(input())
+matrix = [list(map(int, input().split())) for _ in range(n)]
+if is_orthogonal(n, matrix):
+    print("True")
+else:
+    print("False")
