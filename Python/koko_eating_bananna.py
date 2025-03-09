d598a977d8ba3e9d50588e0d71585777a1aa9459
Chanakya Sinde <chanakyasinde@gmail.com>
2025-03-09 23:54:44 +0530

    Update koko_eating_bananna.py

--- Python/koko_eating_bananna.py
+++ Python/koko_eating_bananna.py
@@ -1,1 +1,19 @@
+def cal_time(speed, piles):
+       time=0
+       for pile_size in piles:
+                     time+=(pile_size+speed-1) // speed
+       return time
 
+def minEatingSpeed(piles, h):
+       low = 1
+       high = max(piles)
+       ans = max(piles)
+       while low<=high:
+              mid = (low+high)//2
+              time_taken = cal_time(mid, piles)
+              if time_taken > h:
+                     low = mid + 1
+              else:
+                     ans = mid
+                     high = mid-1
+       return ans
