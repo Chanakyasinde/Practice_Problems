a11470d04ca4f4e6d79a61549bd37547118ae61f
Chanakya Sinde <chanakyasinde@gmail.com>
2025-03-09 23:58:45 +0530

    Update insertion_sort.py

--- Python/insertion_sort.py
+++ Python/insertion_sort.py
@@ -1,1 +1,16 @@
+def outer(lst,i):
+     if i==len(lst):
+          return 
+     inner(lst,i-1,lst[i])
+     outer(lst,i+1)
 
+def inner(lst,j,k):
+     if j<0 or lst[j]<=k:
+          lst[j+1]=k
+          return 
+     lst[j+1]=lst[j]
+     inner(lst,j-1,k)
+
+def recursive_insertion(lst):
+     outer(lst,0)
+     return lst
