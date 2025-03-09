d760e2faf5d35e8c6718a52befa394b673c8eef1
Chanakya Sinde <chanakyasinde@gmail.com>
2025-03-09 23:55:28 +0530

    Update quadratic_equation.py

--- Python/quadratic_equation.py
+++ Python/quadratic_equation.py
@@ -1,1 +1,15 @@
-
+k=int(input())
+low=1
+high=k
+while low<=high:
+    mid=(low+high)//2
+    y=(2*mid*mid)+(5*mid)
+    if y>k:
+        high=mid-1
+    elif y<k:
+        low=mid+1
+    else:
+        print(mid)
+        break
+if high<low:
+    print(-1)
