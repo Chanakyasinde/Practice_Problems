3a52440d6376b86b0ac7bf526463771266446e91
Chanakya Sinde <chanakyasinde@gmail.com>
2025-06-16 08:58:23 +0530

    Update prefix_removal.py

--- Python/prefix_removal.py
+++ Python/prefix_removal.py
@@ -1,1 +1,27 @@
+def longest_prefix(s):
+    n = len(s)
+    for i in range(1, n + 1):
+        prefix = s[:i]
+        if s.find(prefix, i) != -1: 
+            return i
+    return 0
 
+def process_string(s):
+    while True:
+        x = longest_prefix(s)
+        if x == 0:
+            break
+        s = s[x:] 
+    return s
+
+
+t = int(input().strip())
+results = []
+
+for _ in range(t):
+    s = input().strip()
+    result = process_string(s)
+    results.append(result)
+
+for res in results:
+    print(res)
