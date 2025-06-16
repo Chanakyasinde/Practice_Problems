93e2c463eee613554d893342585e4b03435991d4
Chanakya Sinde <chanakyasinde@gmail.com>
2025-06-16 09:00:55 +0530

    Update counter_II.js

--- JavaScript/counter_II.js
+++ JavaScript/counter_II.js
@@ -1,1 +1,25 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let first = init
 
+    function increment() {
+        return ++first
+    }
+    function decrement() {
+        return --first
+    }
+    function reset() {
+        return (first = init)
+    }
+    return {increment, decrement, reset}
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
