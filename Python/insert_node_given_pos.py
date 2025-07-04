905339329ad07c3ec57e2f0215ceb0dbb7907aab
Chanakya Sinde <chanakyasinde@gmail.com>
2025-07-04 12:34:40 +0530

    Update insert_node_given_pos.py

--- Python/insert_node_given_pos.py
+++ Python/insert_node_given_pos.py
@@ -1,1 +1,22 @@
+'''
+    {
+        class Node:
+            def __init__(self, data):   # data -> value stored in node
+                self.data = data
+                self.next = None
+    }
+'''
+def addElement(head, M, K):
+    new_node = Node(K)
 
+    if M==1:
+        new_node.next = head
+        return new_node
+    
+    current = head
+    for _ in range(M-2):
+        current = current.next
+    
+    new_node.next = current.next
+    current.next = new_node
+    return head
