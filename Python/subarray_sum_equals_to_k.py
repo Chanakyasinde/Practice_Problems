eb49b42cbe5b50a8d59ae4b36b71cdb590238b7a
Chanakya Sinde <chanakyasinde@gmail.com>
2025-07-30 18:20:33 +0530

    Update subarray_sum_equals_to_k.py

--- Python/subarray_sum_equals_to_k.py
+++ Python/subarray_sum_equals_to_k.py
@@ -1,1 +1,9 @@
-
+def countSubarraysWithSumK(arr, k):
+    # Your code goes here.
+    num=0
+    n=len(arr)
+    for i in range(n):
+        for j in range(i,n):
+            if sum(arr[i:j+1])==k:
+                num+=1
+    return num
