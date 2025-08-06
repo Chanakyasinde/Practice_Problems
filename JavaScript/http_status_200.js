f79c19c82b6f303f7a44316faee839d8a78c03a8
Chanakya Sinde <chanakyasinde@gmail.com>
2025-08-06 23:40:44 +0530

    Update http_status_200.js

--- JavaScript/http_status_200.js
+++ JavaScript/http_status_200.js
@@ -1,1 +1,13 @@
-
+async function checkStatus(url) {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      return "Success!";
+    } else {
+      return `Error: Received status code ${response.status}`;
+    }
+  } catch (error) {
+    console.error("Network error");
+    return "Network error";
+  }
+}
