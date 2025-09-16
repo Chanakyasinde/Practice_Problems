2fd71c9da602110a394ceac388bbfe16083ad7b5
Chanakya Sinde <chanakyasinde@gmail.com>
2025-09-16 22:31:04 +0530

    Update house_robber.py

--- Python/DP/house_robber.py
+++ Python/DP/house_robber.py
@@ -1,1 +1,13 @@
+def rob(nums):
+    dp = {}
+    return solve(0, nums, dp)
 
+def solve(index, nums, dp):
+    if index >= len(nums):
+        return 0
+    if index in dp:
+        return dp[index]
+    take = nums[index] + solve(index+2, nums, dp)
+    not_take = solve(index+1, nums, dp)
+    dp[index] = max(take, not_take)
+    return dp[index]
