e8502b6ced8c5c206befff5202efbf1387bbcd91
Chanakya Sinde <chanakyasinde@gmail.com>
2025-10-10 22:21:42 +0530

    Update middle_of_pack.py

--- Python/middle_of_pack.py
+++ Python/middle_of_pack.py
@@ -1,1 +1,12 @@
 
+tourist=int(input())
+jiangly=int(input())
+beng=int(input())
+lst=[tourist,jiangly,beng]
+lst.sort()
+if lst[1]==tourist:
+    print("tourist")
+elif lst[1]==jiangly:
+    print("jiangly")
+else:
+    print("benq")
