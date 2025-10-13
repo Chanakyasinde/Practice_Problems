c4fadef7c522a3a9dfa570d2d3158adba4d82036
Chanakya Sinde <chanakyasinde@gmail.com>
2025-10-13 22:01:10 +0530

    Update dfs_of_graph.py

--- Python/Graphs/dfs_of_graph.py
+++ Python/Graphs/dfs_of_graph.py
@@ -1,1 +1,23 @@
+def dfsOfGraph(n, edges):
+    adj = [[] for _ in range(n)]
+    visited = [False] * n
+    lst = []
 
+    for u,v in edges:
+        adj[u].append(v)
+        adj[v].append(u)
+
+    def dfs(node):
+        if visited[node] == True:
+            return 
+        else:
+            visited[node] = True
+            lst.append(node)
+        for neigh in adj[node]:
+            dfs(neigh)
+            
+    for i in range(n):
+        if visited[i] == False:
+            dfs(i)
+    
+    return lst
