7083d689528030c42291ad44b86e4d4da32ad3b0
Chanakya Sinde <chanakyasinde@gmail.com>
2025-10-28 23:23:11 +0530

    Update lowest_increasing_subsequences.py

--- Python/DP/lowest_increasing_subsequences.py
+++ Python/DP/lowest_increasing_subsequences.py
@@ -16,3 +16,14 @@ def lengthOfLIS(nums):
     for i in range(len(nums)):
         answer = max(answer, LIS_ending_at_ith_index(dp, nums, i))
     return answer
+
+# 2nd approach
+
+def lengthOfLIS(nums):
+    n = len(nums)
+    dp = [1]*n
+    for i in range(n):
+        for j in range(i):
+            if nums[i] > nums[j]:
+                dp[i] = max(dp[i], dp[j]+1)
+    return max(dp)
