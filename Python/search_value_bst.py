ed4fdc653a00ea38cb51af2d326c342097abcc58
Chanakya Sinde <chanakyasinde@gmail.com>
2025-12-27 01:33:17 +0530

    Add searchBST function for binary search tree
    Implement search function for BST using inorder traversal.

--- Python/search_value_bst.py
+++ Python/search_value_bst.py
@@ -1,1 +1,24 @@
+"""
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+"""
+def searchBST(root, x):
+    # code here
+    lst = []
+    inorder(root, lst)
+    n = len(lst)
+    for i in range(n):
+        if lst[i] == x:
+            return True
+    return False
 
+
+def inorder(root, lst):
+    if root is None:
+        return 
+    inorder(root.left, lst)
+    lst.append(root.val)
+    inorder(root.right, lst)
