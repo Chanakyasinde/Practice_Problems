3dda54c3a5413124c363ca93774e179486a92ad0
Chanakya Sinde <chanakyasinde@gmail.com>
2026-01-25 00:21:05 +0530

    Add Vehicle interface and service classes for vehicles

--- Typescript/vehicle_service_system.ts
+++ Typescript/vehicle_service_system.ts
@@ -1,1 +1,33 @@
+interface Vehicle {
+  serviceDetails(): void;
+}
 
+class Car {
+    model: string;
+    constructor(model: string) {
+        this.model = model
+    }
+    serviceDetails(): void {
+        console.log(`Car model ${this.model} requires regular servicing`)
+    }
+}
+
+class Bike {
+    engineCC: number
+    constructor(engineCC: number) {
+        this.engineCC = engineCC
+    }
+    serviceDetails(): void {
+        console.log(`Bike with ${this.engineCC}cc engine requires oil change`)
+    }
+}
+
+class Truck {
+    capacity: number
+    constructor(capacity: number){
+        this.capacity = capacity
+    }
+    serviceDetails(): void {
+        console.log(`Truck with capacity ${this.capacity} tons requires heavy maintenance`)
+    }
+}
